2976d90eb4436847c191fd0d8f98400e1110f1a8
Jayden Medeiros <jayden.medeiros@daviestech.org>
2020-10-08 13:48:00 +0000

    submit

---
+++ question1.py
@@ -0,0 +1,4 @@
+y= int(input("Enter an integer: "))
+print(y+1)
+print(y+2)
+print(y+3)
---
+++ question2.py
@@ -0,0 +1,4 @@
+num1=float(input("Enter a decimal: "))
+num2=float(input("Enter another decimal: "))
+num3=float(input("Enter one last decimal: "))
+print(num1 + num2 +num3)
